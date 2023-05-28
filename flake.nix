ade89f2c370271ba5a88989b5f797658405a0226
Seonghyun Park <shp4rk@gmail.com>
2023-05-28 10:43:57 +0000

    x

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Hardware-Assisted Process Tracer for Fuzzing on ARM64 Linux";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "aarch64-linux"
+  ] (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        gcc
+
+        clang-tools
+
+        python3
+      ];
+    };
+  });
+}
